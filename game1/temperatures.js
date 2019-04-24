76df836e0e14a3ac0ab7ad87981ba3d894f69135
karoldylewski <49935530+karoldylewski@users.noreply.github.com>
2019-04-24 11:47:03 +0200

    changed if statement for number of temperatures to analyse

--- game1/temperatures.js
+++ game1/temperatures.js
@@ -1,11 +1,11 @@
 const n = parseInt(readline()); // the number of temperatures to analyse
-var result = 5526;
-if (n==0) result=0;
+var result;
+(n==0) ? result=0 : result=5526;
 var inputs = readline().split(' ');
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < n; i++) { 
     const t = parseInt(inputs[i]);
     if ((Math.abs(t)< Math.abs(result)) || ((Math.abs(t)== Math.abs(result))  && (t>result)) ){
-        result = t;
-    }
+    result = t;
+     }
 }
 console.log(result);

a55e60e7499ebeaccf24eab5149fbbc59bd72661
kdylewski <karol.dylewski@britenet.com.pl>
2019-04-25 10:56:34 +0200

    If-statements amount reduction

--- game2/thor.js
+++ game2/thor.js
@@ -3,8 +3,8 @@ const lightX = parseInt(inputs[0]); // the X position of the light of power
 const lightY = parseInt(inputs[1]); // the Y position of the light of power
 const initialTX = parseInt(inputs[2]); // Thor's starting X position
 const initialTY = parseInt(inputs[3]); // Thor's starting Y position
-var currentTX = initialTX;
-var currentTY = initialTY;
+let currentTX = initialTX;
+let currentTY = initialTY;
 // game loop
 while (true) {
     const remainingTurns = parseInt(readline()); // The remaining amount of turns Thor can move. Do not remove this line.
